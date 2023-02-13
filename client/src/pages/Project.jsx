666d5e994dc57343fccd9614b7372d6e003cf050
Jaydeep <jaydeep.dey03@gmail.com>
2023-02-13 20:08:40 +0530

    Project card fixed and align, made t responsive too

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -13,37 +13,37 @@ const Project = () => {
     return (
         <div className='m-10 font-poppins'>
             <h1 className='text-3xl font-bold text-center mb-10'>Projects</h1>
-            <div className='flex justify-center items-center'>
-                <button className='bg-red-200 rounded-full flex items-center justify-center p-4' onClick={() => swiperRef.current.slidePrev()}>
+            <div className='flex justify-center items-center space-x-6'>
+                <button className='bg-gray-200 rounded-full flex items-center justify-center p-4' onClick={() => swiperRef.current.slidePrev()}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
                     </svg>
                 </button>
                 <Swiper
                     slidesPerView={width < 768 ? 1 : 3}
-                    spaceBetween={30}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    modules={[Pagination, Navigation]}
+                    spaceBetween={10}
+                    pagination={false}
+                    modules={[Navigation, Pagination]}
                     className="mySwiper"
+                    centeredSlides={true}
                     navigation={false}
                     onSwiper={(swiper) => {
                         swiperRef.current = swiper;
                     }}
                 >
+                    {[1, 2, 3, 4, 5, 6].map(() => {
+                        return <SwiperSlide>
+                            <div className='sm:ml-10 p-4 rounded-xl h-1/2 w-80 shadow-lg mt-2 mb-2'>
+                                <img src="/blog1.png" className='rounded-xl' alt="" />
+                                <h3 className='text-center font-light mt-3 text-xl'>Project Title</h3>
+                                <p className='m-4 text-justify max-w-lg text-sm text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum aliquam similique, praesentium esse explicabo, nostrum asperiores ut beatae tempora repellat a facilis</p>
+                            </div>
+                        </SwiperSlide>
+                    })
+                    }
 
-                    <SwiperSlide><div className='h-96 w-96 bg-red-200'>Slide 1</div></SwiperSlide>
-                    <SwiperSlide><div className='h-96 w-96 bg-red-200'>Slide 1</div></SwiperSlide>
-                    <SwiperSlide><div className='h-96 w-96 bg-red-200'>Slide 1</div></SwiperSlide>
-                    <SwiperSlide><div className='h-96 w-96 bg-red-200'>Slide 1</div></SwiperSlide>
-                    <SwiperSlide><div className='h-96 w-96 bg-red-200'>Slide 1</div></SwiperSlide>
-                    <SwiperSlide><div className='h-96 w-96 bg-red-200'>Slide 1</div></SwiperSlide>
-                    <SwiperSlide><div className='h-96 w-96 bg-red-200'>Slide 1</div></SwiperSlide>
-                    <SwiperSlide><div className='h-96 w-96 bg-red-200'>Slide 1</div></SwiperSlide>
-                    <SwiperSlide><div className='h-96 w-96 bg-red-200'>Slide 1</div></SwiperSlide>
                 </Swiper>
-                <button className='bg-red-200 rounded-full flex items-center justify-center p-4' onClick={() => swiperRef.current.slideNext()}>
+                <button className='bg-gray-200 rounded-full flex items-center justify-center p-4' onClick={() => swiperRef.current.slideNext()}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                     </svg>
