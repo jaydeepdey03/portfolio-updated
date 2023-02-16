c67c47ce0652e4010675d18c9c6b14c47f16cd6d
Jaydeep <jaydeep.dey03@gmail.com>
2023-02-16 12:31:32 +0530

    project section done

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -10,17 +10,20 @@ import useDimension from '../hook/useDimension';
 const Project = () => {
     const swiperRef = useRef()
     const { width } = useDimension();
+
+    let text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum aliquam similique, praesentium esse explicabo, nostrum asperiores ut beatae tempora repellat a facilis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem alias modi et, magnam quidem placeat iusto facilis, sed corrupti praesentium ut iste in reiciendis vitae labore molestias delectus tenetur consectetur."
+
     return (
-        <div className='m-10 font-poppins'>
+        <div className='m-4 mb-7 font-poppins'>
             <h1 className='text-3xl font-bold text-center mb-10'>Projects</h1>
-            <div className='flex justify-center items-center space-x-3 md:w-full w-[30rem]'>
+            <div className='flex justify-center items-center space-x-3'>
                 <button className='bg-gray-200 rounded-full flex items-center justify-center p-4' onClick={() => swiperRef.current.slidePrev()}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
                     </svg>
                 </button>
                 <Swiper
-                    slidesPerView={'auto'}
+                    slidesPerView={width < 768 ? '1' : '3'}
                     spaceBetween={20}
                     pagination={false}
                     modules={[Navigation, Pagination]}
@@ -35,10 +38,10 @@ const Project = () => {
                 >
                     {[1, 2, 3, 4, 5, 6].map(() => {
                         return <SwiperSlide>
-                            <div className='sm:ml-10 sm:p-4 w-[18rem] rounded-xl shadow-lg sm:mt-2 sm:mb-2'>
+                            <div className='max-w-sm rounded-xl shadow-lg p-6 m-3'>
                                 <img src="/blog1.png" className='rounded-xl' alt="" />
                                 <h3 className='text-center font-light mt-3 text-xl'>Project Title</h3>
-                                <p className='m-4 text-justify max-w-lg text-sm text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum aliquam similique, praesentium esse explicabo, nostrum asperiores ut beatae tempora repellat a facilis</p>
+                                <p className='text-justify text-sm text-gray-500'>{text.length>20? text.slice(15) + "..." : text}</p>
                             </div>
                         </SwiperSlide>
                     })
